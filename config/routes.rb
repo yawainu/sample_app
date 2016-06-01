63bb0835b1b9850d7d752e0a4b92db1289107a0a
yawainu <yanu@yawainu.com>
2016-06-02 08:59:59 +0900

    add as: rootes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
+  get 'static_pages/help', as: :help
+  get 'static_pages/about', as: :about
+  get 'static_pages/contact', as: :contact
 
   root 'static_pages#home'
 end
